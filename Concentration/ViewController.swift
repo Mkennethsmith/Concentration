72b5227833c92a3a8792a3ea4a8a4ab6f91eda4f
michael.smith <michael.smith@vendigital.com>
2018-03-31 17:02:55 +0100

    First commit, adding cards and basic logic

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Michael Smith on 31/03/2018.
+//  Copyright © 2018 Michael Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["👻","🎃","🙀","🦇","🍬","🍭","🍫","😈","👹"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Card chosen was not in cardButtons array")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+}
+
+
